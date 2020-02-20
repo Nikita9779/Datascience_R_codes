05dc4e4a1cb08c0c1afd987fbbe1f2ff0ecb0a75
Nikita <60732385+Nikita9779@users.noreply.github.com>
2020-02-20 15:20:03 +0530

    Update Linear Regression.R

--- Linear Regression.R
+++ Linear Regression.R
@@ -9,9 +9,9 @@ control<-c(91,87,99,77,88,91)
 Treat<-c(101,110,103,93,99,104)
 t.test(control,Treat,alternative = "two.sided")
 
-
+#to calculate the linear regression
 model<-lm(sunday~daily,data = NewspaperData)
 summary(NewspaperData)
-
+#to predict for 300 daily circulations
 pred<-predict(model,newdata = data.frame(daily=300))
 pred
