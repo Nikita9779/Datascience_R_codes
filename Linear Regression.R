9dd7b1315504452fe08c77d4b0f09cb137ba4837
Nikita <60732385+Nikita9779@users.noreply.github.com>
2020-02-20 15:17:39 +0530

    Add files via upload

---
+++ Linear Regression.R
@@ -0,0 +1,17 @@
+d<-c(101,110,103,93,99,104)
+sd(d) #to calculate standard deviation
+
+
+2*(1-pt(5.57,5))  #to calculate p value 
+
+#to computer two sample test
+control<-c(91,87,99,77,88,91)
+Treat<-c(101,110,103,93,99,104)
+t.test(control,Treat,alternative = "two.sided")
+
+
+model<-lm(sunday~daily,data = NewspaperData)
+summary(NewspaperData)
+
+pred<-predict(model,newdata = data.frame(daily=300))
+pred
